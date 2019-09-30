f519cd81abeee4787eafb091eeb1ab58636f368c
h571544 <fredrik.1997@hotmail.com>
2019-09-30 11:06:55 +0200

    firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { MatTableModule } from "@angular/material/table";
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 import { HttpClientModule } from '@angular/common/http'; 
 
 import { AppComponent } from './app.component';
@@ -32,7 +35,10 @@ import { FooterComponent} from './footer/footer.component';
       {path: '**', redirectTo: 'tabellen', pathMatch: 'full' }
     ]),
     MaterialModule,
-    MatTableModule
+    MatTableModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
